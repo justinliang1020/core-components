253a9872f703c67a1e246a46b8be01b7eb69f8ad
Justin Liang <justinliang1020@gmail.com>
2022-02-09 22:44:18 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -7,8 +7,8 @@ import rollupUrl from '@rollup/plugin-url';
 var componentPath
 var serverPath
 if ((process.env.BUILD !== 'production')) {
-    componentPath = "https://blair-vue-apps.ngrok.io/vue-apps/";
-    serverPath = "https://blair-core.ngrok.io/build/";
+    componentPath = " https://8d7c-66-199-8-132.ngrok.io/vue-apps/";
+    serverPath = "https://a9a3-66-199-8-132.ngrok.io/build/";
 } else {
     componentPath = "https://resources.realitymedia.digital/vue-apps/";
     serverPath = "https://resources.realitymedia.digital/core-components/";
